e5aa037ec481cc4b08f3bd85fc6ed4977766989e
Askarunro <paskar.y.a@gmail.com>
2022-06-23 08:05:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,67 +1,104 @@
 import "./App.css";
 
 
-// import { Routes, Route, Navigate } from "react-router-dom";
-// import { lazy, Suspense } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import Layout from "./components/Layout";
 // import Layout from "./components/Layout";
-
-import ContactForm from "./components/ContactForm";
-import Filter from "./components/Filter";
-import ContactList from "./components/ContactList";
-import { useGetContactsQuery } from "./redux/api/contactsApi"
+// import LoginView from "./views/LoginView";
 
 
+// import ContactForm from "./components/ContactForm";
+// import Filter from "./components/Filter";
+// import ContactList from "./components/ContactList";
+// import { useGetContactsQuery } from "./redux/api/contactsApi"
+
+const LoginView = lazy(() => {
+  return import("./views/LoginView");
+});
+
+  const Register = lazy(() => {
+    return import("./views/RegisterView");
+  });
+
+  const Contacts = lazy(() => {
+    return import("./components/Contacts");
+  });
+
+  const ContactView = lazy(() => {
+    return import("./views/ContactView");
+  });
+  // const ContactList = lazy(() => {
+  //   return import("./components/ContactList");
+  // });
+  // const Filter = lazy(() => {
+  //   return import("./components/Filter");
+  // });
+  const ContactForm = lazy(() => {
+    return import("./components/ContactForm");
+  });
 function App() {
 
-//   const LoginView = lazy(() => {
-//     return import("./views/LoginView");
-//   });
-//   const MovieDetailsPage = lazy(() => {
-//     return import("./components/MovieDetailsPage");
-//   });
-//   const MoviesPage = lazy(() => {
-//     return import("./components/MoviesPage");
-//   });
+
+
+  // const MoviesPage = lazy(() => {
+  //   return import("./components/MoviesPage");
+  // });
   
-//   const Reviews = lazy(() => {
-//     return import("./components/Reviews");
-//   });
-//   const Cast = lazy(() => {
-//     return import("./components/Cast");
-//   });
+  // const Reviews = lazy(() => {
+  //   return import("./components/Reviews");
+  // });
+  // const Cast = lazy(() => {
+  //   return import("./components/Cast");
+  // });
 
 
-//   <Suspense fallback={<div>Loading</div>}>
-//       <Routes>
-//         <Route path="/" element={<Layout />}>
-//           <Route index element={<LoginView />}></Route>
-//           <Route path="/movies" element={<MoviesPage />}></Route>
-//           <Route  path="movies/:movieId" element={<MovieDetailsPage />}>
-//             <Route path="cast" element={<Cast />} />
-//             <Route path="reviews" element={<Reviews />} />
-//           </Route>
-//           {/* <Route path='*' element={<Navigate to='/'/>}></Route> */}
-//           <Route path='*' element={<div><h2>Oops we have a problem</h2><h3>Pages not found</h3></div>}></Route>
-//         </Route>
-//       </Routes>
-//     </Suspense>
 
 
-  const {data: contacts} = useGetContactsQuery();
+
+  // const {data: contacts} = useGetContactsQuery();
 
   return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactForm />
 
-      {contacts && (
-        <>
-          <h2>Contacts</h2>
-          <Filter/>
-          <ContactList />
-        </>
-      )}
-    </div>
+    <Suspense fallback={<div>Loading</div>}>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/users/login" element={<LoginView />}></Route>
+        <Route path="/users/signup" element={<Register />}></Route>
+        <Route path="/contacts" element={<Contacts />}>
+          <Route path="id" element={<ContactView />}>
+          {/* <Route index element={<ContactForm />}/> */}
+          </Route>
+          {/* <Route path="" element={<ContactForm />}></Route>
+          <Route path="" element={<Filter />}></Route>
+          <Route path="" element={<ContactList />}></Route> */}
+        </Route>
+
+        {/* <Route path="/movies" element={<MoviesPage />}></Route>
+        <Route  path="movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path='*' element={<div><h2>Oops we have a problem</h2><h3>Pages not found</h3></div>}></Route> */}
+      <Route path='*' element={<div><h2>Oops we have a problem</h2><h3>Pages not found</h3></div>}></Route>
+      
+      </Route>
+
+    </Routes>
+  </Suspense>
+    // <div>
+    //   <LoginView/>
+    //   {/* <h1>Phonebook</h1>
+    //   <ContactForm />
+
+    //   {contacts && (
+    //     <>
+    //       <h2>Contacts</h2>
+    //       <Filter/>
+    //       <ContactList />
+    //     </>
+    //   )} */}
+    // </div>
   );
 }
 
