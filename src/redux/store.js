c29863654ffb301cf51600a51e5eaf42f94fe92d
Askarunro <paskar.y.a@gmail.com>
2022-06-23 08:06:49 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsApi } from './api/contactsApi';
-import { filterReducer } from "./reduce/filter";
+import { usersApi } from './api/usersApi';
+import { filterReducer, tokenReducer } from "./reduce/filter";
 
 export const store = configureStore({
   reducer: {
     filter: filterReducer,
+    token: tokenReducer,
     [contactsApi.reducerPath]: contactsApi.reducer,
+    [usersApi.reducerPath]: usersApi.reducer,
   },
   middleware: getDefaultMiddleware => [
     ...getDefaultMiddleware(),
     contactsApi.middleware,
+    usersApi.middleware,
   ],
 });
+
+
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import { contactsApi } from './api/contactsApi';
+// // import { api } from './api/usersApi';
+// import { filterReducer, tokenReducer } from "./reduce/filter";
+
+// export const store = configureStore({
+//   reducer: {
+//     filter: filterReducer,
+//     token: tokenReducer,
+//     [contactsApi.reducerPath]: contactsApi.reducer,
+//     // [api.reducerPath]: api.reducer,
+//   },
+//   middleware: getDefaultMiddleware => [
+//     ...getDefaultMiddleware(),
+//     contactsApi.middleware,
+//     // api.middleware,
+//   ],
+// });
+
