28ef519e83e4ad8c19ecd69d10792ae4bafddfdb
Askarunro <paskar.y.a@gmail.com>
2022-06-23 08:06:42 +0300

    Update filter.js

--- src/redux/reduce/filter.js
+++ src/redux/reduce/filter.js
@@ -4,3 +4,8 @@ export const searchContact = createAction("filter/searchContact");
 export const filterReducer = createReducer("", {
   [searchContact]: (state, action) => (state = action.payload),
 });
+
+export const myToken = createAction("token/myToken");
+export const tokenReducer = createReducer(JSON.parse(localStorage.getItem('token')), {
+  [myToken]: (state, action) => (state = action.payload),
+});
