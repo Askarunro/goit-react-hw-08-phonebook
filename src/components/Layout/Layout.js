95d63482beb7932f24f30666f80d7ff1cd511969
Askarunro <paskar.y.a@gmail.com>
2022-06-24 01:27:30 +0300

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,41 @@
 import { Outlet } from "react-router";
 import { Link, NavLink } from "react-router-dom";
 import backImg from "./back.png";
+import { useSelector  } from "react-redux";
+import { useGetUsersQuery } from "..//../redux/api/usersApi";
+
 import l from "./Layout.module.css";
+import UserMenu from '..//UserMenu'
 
 const Layout = () => {
+  const token = useSelector((state) => state.token);
+  // const { data: user } = useGetUsersQuery();
+
   return (
     <>
       <nav className={l.navigate}>
-        <NavLink to="/users/signup" className={l.link}>
+        
+        {!token && <div>
+          <NavLink to="/users/signup" className={l.link}>
           Register
         </NavLink>
         <NavLink to="/users/login" className={l.link}>
           LogIn
         </NavLink>
+        </div>
+        }
       </nav>
+      {token!=="" && <div>
+
+      <NavLink to="/contacts" className={l.link}>
+      contacts
+        </NavLink>
+        <NavLink to="/users/current" className={l.link}>
+        <UserMenu/>
+        </NavLink>
+      
+        </div>}
+      
       <hr />
       <Outlet />
     </>
