1ed6d08fd4b3952d99ff9a3e8e06e45e0da81f1a
Askarunro <paskar.y.a@gmail.com>
2022-06-23 08:06:39 +0300

    Update usersApi.js

--- src/redux/api/usersApi.js
+++ src/redux/api/usersApi.js
@@ -1,9 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export const contactsApi = createApi({
+export const usersApi = createApi({
   reducerPath: 'users',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://connections-api.herokuapp.com',
+    // prepareHeaders: (headers, { getState }) => {
+    //   const token = getState().auth.token
+  
+    //   // If we have a token set in state, let's assume that we should be passing it.
+    //   if (token) {
+    //     headers.set('authorization', `Bearer ${token}`)
+    //   }
+  
+    //   return headers
+    // },
   }),
   tagTypes: ['User'],
   endpoints: builder => ({
@@ -43,4 +53,70 @@ export const {
   useRegisterUserMutation,
   useLoginUserMutation,
   useLogoutUserMutation,
-} = contactsApi;
+} = usersApi;
+
+
+// import { createApi } from '@reduxjs/toolkit/query'
+// import axios from 'axios'
+
+// const axiosBaseQuery =
+//   ({ baseUrl } = { baseUrl: '' }) =>
+//   async ({ url, method, data, params }) => {
+//     try {
+//       const result = await axios({ url: baseUrl + url, method, data, params })
+//       return { data: result.data }
+//     } catch (axiosError) {
+//       let err = axiosError
+//       return {
+//         error: {
+//           status: err.response?.status,
+//           data: err.response?.data || err.message,
+//         },
+//       }
+//     }
+//   }
+
+// const api = createApi({
+//   baseQuery: axiosBaseQuery({
+//     baseUrl: 'https://connections-api.herokuapp.com',
+//   }),
+//   endpoints(build) {
+//     return {
+//       query: build.query({ query: () => ({ url: '/users/current', method: 'get' }) }),
+//       mutation: build.mutation({
+//         query: () => ({ url: '/users/login', method: 'post' }),
+//         query: () => ({ url: `/users/logout`, method: 'post' }),
+//         query: () => ({ url: `/users/signup`, method: 'post' }),
+
+
+//       }),
+//       // mutation: build.mutation({
+//       //   query: () => ({ url: `/users/logout`, method: 'post' }),
+//       // }),
+//       // mutation: build.mutation({
+//       //   query: () => ({ url: `/users/signup`, method: 'post' }),
+//       // }),
+//     }
+//     // return {
+//     //   query: build.query({ query: () => ({ url: '/contacts', method: 'get' }) }),
+//     //   mutation: build.mutation({
+//     //     query: () => ({ url: '/contacts', method: 'post' }),
+//     //   }),
+//     //   mutation: build.mutation({
+//     //     query: (id) => ({ url: `/contacts/${id}`, method: 'post' }),
+//     //   }),
+//     //   mutation: build.mutation({
+//     //     query: (id) => ({ url: `/contacts/${id}`, method: 'patch' }),
+//     //   }),
+//     // }
+//   },
+// })
+
+// // export const {
+// //   useGetUsersQuery,
+// //   useRegisterUserMutation,
+// //   useLoginUserMutation,
+// //   useLogoutUserMutation,
+// // } = axiosBaseQuery;
+
+// export default api
