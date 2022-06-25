73cd8cf9930d14bb4bde33140fd2e911cb78c775
Askarunro <paskar.y.a@gmail.com>
2022-06-25 17:39:51 +0300

    Update Contacts.js

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,40 +1,30 @@
-// import f from "./ContactForm.module.css";
-import { useState } from "react";
-
-import { Outlet } from "react-router";
-import { useSelector  } from "react-redux";
-import { useGetContactsQuery } from "..//..//redux/api/contactsApi"
+import c from "./Contacts.module.css";
+import { useSelector } from "react-redux";
+import { useAddContactMutation } from "..//..//redux/api/contactsApi";
 
 import ContactForm from "../ContactForm";
 import Filter from "../Filter";
 import ContactList from "../ContactList/";
 
-
-// import { lazy, Suspense } from "react";
-
-
-// const ContactsList = lazy(() => {
-//   return import("./component/ContactsList");
-// });
-// const Filter = lazy(() => {
-//   return import("./component/Filter");
-// });
-// const ContactForm = lazy(() => {
-//   return import("./component/ContactForm");
-// });
+import { Grid, Container } from "@mui/material";
 
 function Contacts() {
-  const token = useSelector((state) => state.token);
-  
+  const [addContact] = useAddContactMutation();
   return (
-    <div>
-    <h1>Phonebook</h1>
-    <h2>New contact</h2>
-    <ContactForm/>
-    <h2>Contacts</h2>
-    <Filter/>
-    <ContactList/>
-  </div>
+    <Container>
+      <h1>Phonebook</h1>
+      <Grid container direction="row" justifyContent="start" alignItems="start" gap={12}>
+        <Grid>
+          <h2>Contacts list</h2>
+          <Filter />
+          <ContactList />
+        </Grid>
+        <Grid>
+          <h2 className={c.title}>New contact</h2>
+          <ContactForm btnTitle={`Add contact`} addContact={addContact}/>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
