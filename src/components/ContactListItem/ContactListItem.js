0d45fbea3a90672aea21b4c5390447eb61fcd147
Askarunro <paskar.y.a@gmail.com>
2022-06-25 17:40:40 +0300

    Update ContactListItem.js

--- src/components/ContactListItem/ContactListItem.js
+++ src/components/ContactListItem/ContactListItem.js
@@ -1,37 +1,31 @@
 import i from "./ContactListItem.module.css";
-import { useSelector  } from "react-redux";
-import { NavLink, useSearchParams} from "react-router-dom";
-import { useEffect } from "react";
-import { useDeleteContactMutation, useGetContactsQuery } from "..//../redux/api/contactsApi"
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { useDeleteContactMutation, useGetContactsQuery } from "..//../redux/api/contactsApi";
+import { Button, ListItem } from "@mui/material";
+import { DeleteForever } from "@mui/icons-material";
 
 function Item() {
   const filter = useSelector((state) => state.filter);
   const [deleteContact] = useDeleteContactMutation();
-  const {data: contacts } = useGetContactsQuery();
-  const randomColor = Math.floor(Math.random()*16777215).toString(16);
- 
+  const { data: contacts } = useGetContactsQuery();
+
   return (
     <>
-      {contacts && (contacts
-        .filter((option) =>
-          option.name.toLowerCase().includes(filter.toLowerCase())
-        )
-        .map((contact) => (
-          
-          <li key={contact.name} className={i.item} data-id={contact.name}>
-            
-            <NavLink to={`/contacts/${contact.id}`} className={i.item}>
-            <div ></div>
-            <p>{contact.name}:</p>
-            <p>{contact.number}</p>
+      {contacts &&
+        contacts
+          .filter((option) => option.name.toLowerCase().includes(filter.toLowerCase()))
+          .map((contact) => (
+            <ListItem key={contact.name} className={i.item} data-id={contact.name}>
+              <NavLink to={`/contacts/${contact.id}`} className={i.link}>
+                <p>name: {contact.name}:</p>
+                <p>number: {contact.number}</p>
               </NavLink>
-            {/* <button type="button" 
-            onClick={()=>deleteContact(contact.id)} 
-            className={i.btn}>
-              Delete
-            </button> */}
-          </li>
-        )))}
+              <Button type="button" onClick={() => deleteContact(contact.id)} className={i.btn} variant="outlined" color="error" size="large">
+                <DeleteForever />
+              </Button>
+            </ListItem>
+          ))}
     </>
   );
 }
