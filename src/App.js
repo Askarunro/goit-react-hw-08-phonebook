3d3a2be67452a906c91ee508e5b36feedec6e6fd
Askarunro <paskar.y.a@gmail.com>
2022-06-26 16:55:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
-
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Layout from "./components/Layout";
 import { useSelector } from "react-redux";
@@ -22,21 +21,17 @@ const ContactView = lazy(() => {
 });
 
 function App() {
+  const shouldRedirect = true;
   const token = useSelector((state) => state.token);
-
+  console.log(token);
   return (
     <Suspense fallback={<div>Loading</div>}>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/users/login" element={<LoginView />}></Route>
-          <Route path="/users/signup" element={<Register />}></Route>
-          {token && (
-            <>
-            <Route path="/contacts" element={<Contacts />}>
-            </Route>
-            <Route path="contacts/:id" element={<ContactView />} />
-            </>
-          )}
+          <Route path="/users/login" element={token ? <Navigate replace to="/contacts" /> : <LoginView />} />
+          <Route path="/users/signup" element={token ? <Navigate replace to="/contacts" /> : <Register />} />
+          <Route path="/contacts" element={token === "" ? <Navigate replace to="/" /> : <Contacts />} />
+          <Route path="contacts/:id" element={token === "" ? <Navigate replace to="/" /> : <ContactView />} />
           <Route
             path="*"
             element={
