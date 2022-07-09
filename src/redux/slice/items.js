2bec7ffcfc7113f5e02df465c020018a59f54a5f
Askarunro <paskar.y.a@gmail.com>
2022-07-09 19:08:48 +0300

    Update items.js

--- src/redux/slice/items.js
+++ src/redux/slice/items.js
@@ -1,8 +1,31 @@
+// import { createSlice } from '@reduxjs/toolkit'
+// import { PayloadAction } from '@reduxjs/toolkit'
+// import { User } from '..//api/usersApi'
+
+// const slice = createSlice({
+//   name: 'auth',
+//   initialState: { user: null, token: null } ,
+//   reducers: {
+//     setCredentials: (
+//       state,
+//       { payload: { user, token } }
+//     ) => {
+//       state.user = user
+//       state.token = token
+//     },
+//   },
+// })
+
+
 import { createSlice } from "@reduxjs/toolkit";
 
 export const authSlice = createSlice({
   name: "auth",
-  initialState:{},
+  initialState:{
+    user: { name: null, email: null },
+    token: localStorage.getItem("token") || "",
+  isLoggedIn: false,
+  },
   initialState: {
     token: localStorage.getItem("token") || "",
   },
@@ -14,7 +37,7 @@ export const authSlice = createSlice({
   extraReducers: (build) => {
     console.log(build)
     build.addMatcher(
-      authApi.endpoints.login.matchFulfilled,
+      // authApi.endpoints.login.matchFulfilled,
       (state, { payload }) => {
         state.token = payload.token;
       }
@@ -23,6 +46,11 @@ export const authSlice = createSlice({
 });
 
 export const { logout } = authSlice.actions;
+
+
+
+
+
 
 // import { createAction, createReducer} from "@reduxjs/toolkit";
 
